a8c84d0b0b5282d793ceede1a5cdb2f25c86edba
Shant Parseghian <shantp@gmail.com>
2015-08-07 20:24:13 -0700

    Added eslint

--- src/app.jsx
+++ src/app.jsx
@@ -1,14 +1,12 @@
 import 'babel-core/polyfill';
 import React from 'react';
 
-const DOM_APP_ID = 'app';
-
 class App extends React.Component {
   render() {
     return (
       <div />
-    )
+    );
   }
 }
 
-React.render(<App />, document.getElementById(DOM_APP_ID));
+React.render(<App />, document.getElementById('app'));
