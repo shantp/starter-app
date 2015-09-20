b257de8ec722a8e3b64e89f7d60fb7b2f434cc4b
Shant Parseghian <shantp@gmail.com>
2015-09-19 21:37:29 -0700

    Added dev-tools and slider-monitor

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import React from 'react';
 import {Provider} from 'react-redux';
 import App from './App';
 import configureStore from './store/configureStore';
+import {DevTools} from 'redux-devtools/lib/react';
+import SliderMonitor from 'redux-slider-monitor';
 
 const store = configureStore();
 
 React.render(
-  <Provider store={store}>
-    {() => <App />}
-  </Provider>,
+  <div>
+    <Provider store={store}>
+      {() => <App />}
+    </Provider>
+    <DevTools store={store} monitor={SliderMonitor} />
+  </div>,
   document.getElementById('root')
 );
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,10 +1,14 @@
-import {createStore, applyMiddleware} from 'redux';
+import {compose, createStore, applyMiddleware} from 'redux';
 import createLogger from 'redux-logger';
+import {devTools} from 'redux-devtools';
 import rootReducer from '../reducers';
 
 const logger = createLogger();
 
-const createStoreWithMiddleware = applyMiddleware(logger)(createStore);
+const createStoreWithMiddleware = compose(
+  applyMiddleware(logger),
+  devTools()
+)(createStore);
 
 export default function configureStore(initialState) {
   const store = createStoreWithMiddleware(rootReducer, initialState);
