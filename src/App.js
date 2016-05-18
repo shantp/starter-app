0defacd8ae4a8a0ba42e3f753557d7af76a4dd6a
Shant Parseghian <shant@goguardian.com>
2016-05-18 16:56:44 -0700

    :)

--- src/App.js
+++ src/App.js
@@ -1,33 +1,31 @@
 import React, {PropTypes} from 'react';
-import {bindActionCreators} from 'redux';
-import {connect} from 'react-redux';
-import Tabs from './components/Tabs';
-import * as ActionCreators from './actions/tabs';
+import {VictoryArea} from 'victory';
+import _ from 'lodash';
 
-class App extends React.Component {
+export default class App extends React.Component {
+  getData() {
+    return _.map(_.range(5), (index) => {
+      return [
+        {x: "apples", y: _.random(1, 1155)},
+        {x: "oranges", y: _.random(1, 100)},
+        {x: "bananas", y: _.random(1, 10)},
+        {x: "peaches", y: _.random(1, 5)},
+        {x: "pears", y: _.random(1, 5)}
+      ];
+    });
+  }
+
   render() {
-    const {tabs, dispatch} = this.props;
-    const actions = bindActionCreators(ActionCreators, dispatch);
-
+    const data = this.getData();
+    console.log(data);
     return (
       <div>
-        <Tabs
-          tabs={tabs}
-          actions={actions} />
+        <VictoryArea
+          stacked
+          height={600}
+          padding={75}
+          data={data} />
       </div>
     );
   }
 }
-
-App.propTypes = {
-  tabs: PropTypes.object.isRequired,
-  dispatch: PropTypes.func.isRequired
-};
-
-function mapStateToProps(state) {
-  return {
-    tabs: state.tabs
-  };
-}
-
-export default connect(mapStateToProps)(App);
--- src/reducers/tabs.js
+++ src/reducers/tabs.js
@@ -1,14 +1,9 @@
 import {NEW_TAB, UPDATE_TAB} from '../actions/actionTypes';
-import Immutable from 'immutable';
 
-const initialState = Immutable.List([]);
+const initialState = {}
 
 export function tabs(state = initialState, action) {
   switch (action.type) {
-    case NEW_TAB:
-      return state.push(action.tab);
-    case UPDATE_TAB:
-      return state.set(action.id, action.tab);
     default:
       return state;
   }
